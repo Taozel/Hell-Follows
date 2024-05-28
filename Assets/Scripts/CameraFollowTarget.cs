75e9c4c0932e39335c02109a8ed019d04eb7141f
LUF-FY <75016792+LUF-FY@users.noreply.github.com>
2024-05-28 22:33:44 +0300

    hideWalls

--- Assets/Scripts/CameraFollowTarget.cs
+++ Assets/Scripts/CameraFollowTarget.cs
@@ -9,11 +9,14 @@ public class CameraFollowTarget : MonoBehaviour
     public Vector3 offsetPos;
     public float moveSpeed = 5;
     public float smooth = 0.2f;
+    public string WallsTag;
     private Vector3 velocity = Vector3.zero;
+    public Camera cam;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -26,5 +29,27 @@ public class CameraFollowTarget : MonoBehaviour
         targetPos = target.transform.position + offsetPos;
         //transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime* smooth);
         transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smooth);
+    }
+
+    private void Update()
+    {
+        HideWall();
+        Debug.Log("Update");
+    }
+
+    private void HideWall()
+    {
+        Debug.Log("HideWall");
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var go = hit.collider.gameObject;
+            Debug.Log(go.tag);
+            if (go.tag == WallsTag)
+            {
+                go.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
     }
 }
---
+++ Assets/Wall.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private MeshRenderer renderer;
+    private bool isCorutineStarted = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (renderer != null && !renderer.enabled)
+        {
+            if(!isCorutineStarted)
+                StartCoroutine(EnableWall());
+        }
+    }
+
+
+
+    private IEnumerator EnableWall()
+    {
+        isCorutineStarted = true;
+        yield return new WaitForSeconds(10);
+        renderer.enabled = true;
+        isCorutineStarted = false;
+    }
+}
